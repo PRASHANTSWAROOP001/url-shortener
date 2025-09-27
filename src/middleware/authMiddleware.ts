762b50c5475df98c426379f0b8430901faaa2e4c
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-09-28 00:19:16 +0530

    added auth for verification but not tested.

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,36 @@
+import type { Next, Context } from "hono";
+import {verify} from "hono/jwt"
+import dotenv from "dotenv"
+dotenv.config();
+
+export const authMiddleware = async (c:Context, next:Next)=>{
+    try {
+
+        const header = c.req.header("Authorization")
+
+        if(!header || header.startsWith("Bearer")){
+            return c.json({
+                success:false,
+                message:"missing auth tokens",
+            },401)
+        }
+
+        const token = header.split(" ")[1]
+
+        const decoded = verify(token, process.env.JWT_SECRET!) 
+
+        c.set("user",decoded)
+
+        await next();
+        
+    } catch (error) {
+
+        console.error("error while validating token at the middleware.", error)
+
+        return c.json({
+            success:false,
+            message:"expired/invalid token provided",
+        },401)
+        
+    }
+}
